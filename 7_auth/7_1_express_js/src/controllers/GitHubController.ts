3a42c1d4fdcfe38b9f69a7fd0a9ae2503f6a354e
이은상 <kyrsang0801@inbrein.com>
2024-12-26 17:14:43 +0900

    chore:

--- 7_auth/7_1_express_js/src/controllers/GitHubController.ts
+++ 7_auth/7_1_express_js/src/controllers/GitHubController.ts
@@ -76,7 +76,7 @@ export class GithubController {
             await redis.set(`user:${userInfo.login}:access_token`, access_token);
 
             res.cookie('is_authenticated', 'true', {
-                secure: true,
+                // secure: true,
                 sameSite: "none",
                 maxAge: 3_600_000  // 1 hour
             });
